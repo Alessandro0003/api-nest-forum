b107070ccf9f27cae45cde87da33125adf0f41f8
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-23 02:28:33 -0300

    🔥 feat: creating controller for create questions

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,51 @@
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prismaService: PrismaService) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+
+    const slug = this.convertToSlug(title)
+
+    await this.prismaService.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
+  }
+}
