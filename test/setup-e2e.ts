18f230ed8d7f4138ee0098e7b05074022d865d81
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-25 01:39:11 -0300

    🔥 feat: creating setup for isolated database in tests

---
+++ test/setup-e2e.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseUrl(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable.')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseUrl(schemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
