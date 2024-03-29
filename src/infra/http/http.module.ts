7a88770dda4c151c48be25d5d2c6a5251bee8a3f
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-28 21:22:04 -0300

    🔥 feat: decoupling layes in application

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
-import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env'
-import { AuthModule } from './auth/auth.module'
 import { AuthenticateAccountController } from './controllers/authenticate-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-question.controller'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validate: (env) => envSchema.parse(env),
-      isGlobal: true,
-    }),
-    AuthModule,
-  ],
   controllers: [
     CreateAccountController,
     AuthenticateAccountController,
@@ -24,4 +14,4 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
   ],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class HttpModule {}
