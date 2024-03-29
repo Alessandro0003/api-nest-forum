58ca05536a56ff3a1c9281001c81bd206b70e66d
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-28 21:50:45 -0300

    🔥 feat: creating layer PrismaRepository for all services forum

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachment-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswersCommentsRepository } from './prisma/repositories/prisma-answers-comments-repository'
+import { PrismaAnswersAttachmentRepository } from './prisma/repositories/prisma-answers-attachment-repository'
+
+@Module({
+  providers: [
+    PrismaService,
+    PrismaQuestionRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswersCommentsRepository,
+    PrismaAnswersAttachmentRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswersCommentsRepository,
+    PrismaAnswersAttachmentRepository,
+  ],
+})
+export class DatabaseModule {}
