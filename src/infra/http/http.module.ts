77111b715dda9bce46098aa4b04b613e093dcc37
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-28 21:49:18 -0300

    :recycle: refactor: move prismaService to layer databaseModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,15 +3,16 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { AuthenticateAccountController } from './controllers/authenticate-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-question.controller'
-import { PrismaService } from '../prisma/prisma.service'
+import { PrismaService } from '../database/prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateAccountController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [PrismaService],
 })
 export class HttpModule {}
