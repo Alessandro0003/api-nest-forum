e52c5ffcc4d7b81fceeccb8af4c8587ecbcf97e6
Alessandro0003 <alessandroquinta3@gmail.com>
2024-04-10 22:56:29 -0300

    🔥 feat: add field email and password in layer entities student and creating new StudentsRepository

---
+++ src/domain/forum/application/repositories/students-repository.ts
@@ -0,0 +1,7 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { Student } from '../../enterprise/entities/student'
+
+export abstract class StudentsRepository {
+  abstract findByEmail(email: string): Promise<Student | null>
+  abstract create(student: Student): Promise<void>
+}
