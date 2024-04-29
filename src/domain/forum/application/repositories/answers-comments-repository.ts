d4a712a4275e32d3449f34821e4af63ca533ca3e
Alessandro0003 <alessandroquinta3@gmail.com>
2024-04-29 17:16:13 -0300

    :recycle: refactor: change interface for abastract class

--- src/domain/forum/application/repositories/answers-comments-repository.ts
+++ src/domain/forum/application/repositories/answers-comments-repository.ts
@@ -1,12 +1,13 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
-export interface AnswersCommentsRepository {
-  findById(id: string): Promise<AnswerComment | null>
-  findManyByAnswerId(
+export abstract class AnswersCommentsRepository {
+  abstract findById(id: string): Promise<AnswerComment | null>
+  abstract findManyByAnswerId(
     answerId: string,
     params: PaginationParams,
   ): Promise<AnswerComment[]>
-  create(answerComment: AnswerComment): Promise<void>
-  delete(answerComment: AnswerComment): Promise<void>
+
+  abstract create(answerComment: AnswerComment): Promise<void>
+  abstract delete(answerComment: AnswerComment): Promise<void>
 }
