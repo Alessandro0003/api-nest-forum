cf28eafe580e65f9edd9b314332ee794ec69c43e
Alessandro0003 <alessandroquinta3@gmail.com>
2024-05-02 00:34:39 -0300

    :fireworks: feat: creating method for DeleteAnswerComment and add Injectable in DeleteAnswerCommentUseCase for implementation in HttpModule

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -2,6 +2,7 @@ import { right, left, Either } from '@/core/either'
 import { AnswersCommentsRepository } from '../repositories/answers-comments-repository'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteCommentAnswerUseCaseRequest {
   authorId: string
@@ -13,6 +14,7 @@ type DeleteCommentAnswerUseCaseResponse = Either<
   null
 >
 
+@Injectable()
 export class DeleteCommentAnswerUseCase {
   constructor(
     private readonly answerCommentsRepository: AnswersCommentsRepository,
