91ec8433797996caaec0e4b2d57837c852a22144
Alessandro0003 <alessandroquinta3@gmail.com>
2024-05-04 00:08:19 -0300

    :fireworks: feat: creating settings for R2Storage and create StorageModule

---
+++ src/infra/storage/r2-storage.ts
@@ -0,0 +1,51 @@
+import {
+  UploadParams,
+  Uploader,
+} from '@/domain/forum/application/storage/uploader'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { EnvService } from '../env/env.service'
+import { randomUUID } from 'node:crypto'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class R2Storage implements Uploader {
+  private client: S3Client
+
+  constructor(private envService: EnvService) {
+    const accountId = envService.get('CLOUDFLARE_ACCOUNT_ID')
+
+    this.client = new S3Client({
+      endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
+      region: 'auto',
+      credentials: {
+        accessKeyId: envService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: envService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    })
+  }
+
+  async upload({
+    fileName,
+    fileType,
+    body,
+  }: UploadParams): Promise<{ url: string }> {
+    const uploadId = randomUUID()
+
+    const uniqueFileName = `${uploadId}-${fileName}`
+
+    await this.client.send(
+      new PutObjectCommand({
+        Bucket: this.envService.get('AWS_BUCKET_NAME'),
+        Key: uniqueFileName,
+        ContentType: fileType,
+        Body: body,
+      }),
+    )
+
+    // save the reference and don't save the entire URL in the database
+
+    return {
+      url: uniqueFileName,
+    }
+  }
+}
