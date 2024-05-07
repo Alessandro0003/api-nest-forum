d470bad12cfab1aa6d03fc2a95f5b70aef803256
Alessandro0003 <alessandroquinta3@gmail.com>
2024-05-07 20:59:42 -0300

    :sparkles: feat: create PrismaAttachmentsRepository

---
+++ src/infra/database/prisma/repositories/prisma-attachments-repository.ts
@@ -0,0 +1,18 @@
+import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository'
+import { Attachment } from '@/domain/forum/enterprise/entities/attachment'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaAttachmentMapper } from '../mappers/prisma-attachments-mapper'
+
+@Injectable()
+export class PrismaAttachmentsRepository implements AttachmentsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(attachment: Attachment): Promise<void> {
+    const data = PrismaAttachmentMapper.toPrisma(attachment)
+
+    await this.prisma.attachment.create({
+      data,
+    })
+  }
+}
