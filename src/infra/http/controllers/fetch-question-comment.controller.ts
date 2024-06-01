725fc605fcab979707f1fdc1e361cfe0130bd163
Alessandro0003 <alessandroquinta3@gmail.com>
2024-06-01 01:43:58 -0300

    :bug: fix: change code to adding method CommentWithAuthorPresenter

--- src/infra/http/controllers/fetch-question-comment.controller.ts
+++ src/infra/http/controllers/fetch-question-comment.controller.ts
@@ -9,6 +9,7 @@ import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { z } from 'zod'
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
 import { CommentPresenter } from '../presenters/comment-presenter'
+import { CommentWithAuthorPresenter } from '../presenters/commet-with-author-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -36,8 +37,8 @@ export class FetchQuestionCommentController {
       throw new BadRequestException()
     }
 
-    const questionComments = result.value.questionComments
+    const comments = result.value.comments
 
-    return { comments: questionComments.map(CommentPresenter.toHTTP) }
+    return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) }
   }
 }
