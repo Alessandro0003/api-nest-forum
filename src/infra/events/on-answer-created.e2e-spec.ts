6f18986b092af228b3ad277ad0409b9d78ce9a0a
Alessandro0003 <alessandroquinta3@gmail.com>
2024-07-17 00:57:11 -0300

    :sparkles: feat: add method to tests run DomainEvents

--- src/infra/events/on-answer-created.e2e-spec.ts
+++ src/infra/events/on-answer-created.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { AppModule } from '@/infra/app.module'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { PrismaService } from '@/infra/database/prisma/prisma.service'
@@ -29,6 +30,8 @@ describe('On answer created (E2E)', () => {
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
     jwt = moduleRef.get(JwtService)
+
+    DomainEvents.shouldRun = true
 
     await app.init()
   })
